df4705ee28d783f05601f8a3297bc4cea5a6fe10
kindly <kindly@gmail.com>
2013-05-31 11:12:15 +0100

    add run.py script

---
+++ run.py
@@ -0,0 +1,27 @@
+import subprocess
+import random
+import string
+import sys
+
+if len(sys.argv) != 4:
+    print 'run username vmname cert'
+    sys.exit(1)
+
+password = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(70)) + '?'
+
+user, name, cert = sys.argv[1], sys.argv[2], sys.argv[3]
+
+subprocess.call(
+["azure",  "vm",  "create",  name,  "ckandb",  user, password, "-n", name + "db" , "-e", "50022", "-t", cert, "--location", "West Europe"]
+)
+
+subprocess.call(
+["azure",  "vm",  "create",  name,  "ckanweb", user, password, "-n", name + "web", "-e", "-t", cert, "--location", "West Europe", "-c"]
+)
+
+subprocess.call(
+["azure", "vm", "endpoint", "create", name+"web", "80", "80"]
+)
+
+
+
